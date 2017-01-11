17818c2210c478ff4e83618a1f4acea63d9b94e1
Michael Jach <miyu.46300@gmail.com>
2017-01-11 19:41:09 +0000

    Pages v1.0.0

---
+++ config.rb
@@ -0,0 +1,11 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'assets/stylesheets/css'
+sass_dir = 'assets/stylesheets/sass'
+fonts_dir = 'assets/fonts'
+fonts_path = 'assets/fonts'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/scripts'
+relative_assets = true
+line_comments = false
+output_style = :compact
